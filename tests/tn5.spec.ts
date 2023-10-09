6854f7f774a4dca60b5c6cf81616883a35fb93d7
gequise <gustavo.equise@gmail.com>
2023-10-09 00:49:25 -0300

    first commit

---
+++ tests/tn5.spec.ts
@@ -0,0 +1,334 @@
+import { test, expect } from '@playwright/test';
+
+test('has title 1', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+  await page.frameLocator('.brick_full > iframe').getByText('¡Gracias por votar!', { exact: true }).isVisible()
+
+});
+
+test('has title 2', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('has title 3', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('has title 4', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+
+test('has title 5', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('has title 6', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('has title 7', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('has title 8', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('has title 9', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('has title 10', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+  // await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('has title 11', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 12', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 13', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 14', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 15', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 16', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 17', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 18', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 19', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 20', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 21', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 22', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 23', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 24', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 25', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 26', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 27', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 28', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 29', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 30', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 31', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 32', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 33', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 34', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 35', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 36', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 37', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 38', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 39', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 40', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 41', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 42', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 43', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 44', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 45', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 46', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 47', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 48', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+
+test('has title 49', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
+
+test('has title 50', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#adzone_ittIcon_ad-slot-itt').getByRole('img').click();
+  await page.frameLocator('.brick_full > iframe').locator('div:nth-child(4) > ._2iOcrrk9I4LxMSZo502VM6 > .image').click();
+});
